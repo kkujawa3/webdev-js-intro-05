b55bf05e747e997d4bfbd78fc402383ff6d5c06d
unknown <kimberllee.al@gmail.com>
2025-01-10 15:31:04 -0500

    Completed JS part 1 and part 2 assignment.

--- js/assignment.js
+++ js/assignment.js
@@ -9,7 +9,8 @@ const todos = [
 ];
 
 // Declare your two immutable variables
-
+let shiftValue;
+let popValue;
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
@@ -22,43 +23,51 @@ const answerSixEl = document.getElementById("answer-six");
 const submissionBtn = document.getElementById("submission-btn");
 
 function updateAnswerOne() {
-    
+    answerOneEl.textContent = [todos];
 }
 
 function updateAnswerTwo() {
-
+    answerTwoEl.textContent = [todos [2]];
 }
 
 function removeLastValue() {
-
+    popValue = todos.pop();
 }
 
 function removeFirstValue() {
-
+    shiftValue = todos.shift();
 }
 
 function addShiftAndPopValues() {
-
+    todos.push(shiftValue, popValue);
 }
 
 function updateAnswerFour() {
-
+    answerFourEl.textContent = [todos];
 }
 
 function reverseTodoList() {
-
+    todos.reverse();
 }
 
 function updateAnswerFive() {
-
+    answerFiveEl.textContent = [todos];
 }
 
 function updateAnswerSix() {
-
+    answerSixEl.textContent = [todos];
 }
 
 function render() {
-
+    updateAnswerOne();
+    updateAnswerTwo();
+    removeLastValue();
+    updateAnswerFour();
+    reverseTodoList();
+    updateAnswerFive();
+    removeFirstValue();
+    addShiftAndPopValues();
+    updateAnswerSix();
 }
 
 submissionBtn.addEventListener("click", function () {
--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+let mixedArray = [32, "hi", false, 770, [1, 2, 3]];
+console.log(mixedArray);                // output: [32, 'hi', false, 770, Array(3)]
+// outputs a number, string, boolean, number and array with 3 elements
+
+let differentArray = [ 10.1, "coding", true, [18, 3, 20, 9]];
+console.log(differentArray);            // output: [32, 'coding', true, Array(4)]
+// outputs a number, string, boolean, and array with 4 elements
+
+console.log(typeof 10.1);               // output: number
+console.log(typeof "coding");           // output: string
+console.log(typeof true);               // output: boolean
+console.log(typeof [18, 3, 20, 9]);     // output: object 
+
+mixedArray.shift();
+console.log(mixedArray);                // output: ['hi', false, 770, Array(3)] - removed 32
+
+differentArray.pop();
+console.log(differentArray);            // output: [10.1, 'coding', true] - removed [18, 3, 20, 9]
+
+mixedArray.push("Welcome");
+console.log(mixedArray);                // output: ['hi', false, 770, Array(3), 'Welcome'] - adds Welcome to the array
+
+differentArray.push([10, 24]);
+console.log(differentArray);            // output: [10.1, 'coding', true, Array(2)] - adds an array of (2) to the array
+
+mixedArray[0] = "hiya";                 // output: ['hiya', false, 770, array(3), 'Welcome] - changes "hi" to "hiya"
+console.log(mixedArray);
+
+differentArray[3] = [12, 30, 66, 15];   // output: [10.1, 'coding', true, Array(4)] - changes the Array of (2) to (4)
+console.log(differentArray);
+*/
